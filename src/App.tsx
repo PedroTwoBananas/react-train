180e339d03f4f2737c3f09175052c181feb597a3
PedroTwoBananas <paradaigor20@gmail.com>
2022-02-10 12:07:51 +0300

    Создание react-приложения TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { render } from 'react-dom';
 
+
+class App extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <h1>Привет!</h1>
+        )
+
+    }
+}
+
+export default App;
 
 
 
