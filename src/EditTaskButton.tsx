e20a149f3bf1efbf6056fc5eaf604ed12cd173d2
PedroTwoBananas <paradaigor20@gmail.com>
2022-02-10 15:18:23 +0300

    Кнопка изменения задачи

---
+++ src/EditTaskButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class EditTaskButton extends React.Component {
+    render(): React.ReactNode {
+        return(
+            <button>Изменить задачу</button>
+        )
+    }
+}
+
+export default EditTaskButton;
