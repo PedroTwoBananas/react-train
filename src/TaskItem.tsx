eb200f90b0337b24e8ebd92e299eb5d58d4b3f8d
PedroTwoBananas <paradaigor20@gmail.com>
2022-02-10 15:11:33 +0300

    Реализация блока задачи

---
+++ src/TaskItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TaskItem extends React.Component {
+    render(): React.ReactNode {
+        return(
+            <div>
+                <span></span>
+            </div>
+        )
+    }
+}
+
+export default TaskItem;
